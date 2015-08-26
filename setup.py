b373f1a145542d58c85655ece1ab812163b0afcb
Matt Hill <mhill92@gmail.com>
2015-08-25 22:10:39 -0400

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from numpy.distutils.core import Extension, setup
+from distutils.core import setup, Extension
+import numpy
 
-setup(name='drwfast',
-	packages=["drwfast"],
-       ext_modules=[Extension(name='drwfast.tridiagonal', sources=['./drwfast/tridiagonal.f90'])],
-       )
+setup(name='tri_utils',
+	packages=['tri_utils'],
+    ext_modules=[Extension(name="tri_utils._tridiagonal", sources=["./tri_utils/_tridiagonal.c", "./tri_utils/tridiagonal.c"])],
+    include_dirs=[numpy.get_include()],
+)
